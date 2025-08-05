218f9c55bf0cecccc4dd2b29c8d9462944057648
Steward Jimenez <steawrdjm@hotmail.es>
2025-08-04 18:41:29 -0600

    Corregir ruta del ABI

--- src/GAIAContract.js
+++ src/GAIAContract.js
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
-import GAIA_ABI from "./GAIA_ABI.json"; // Ajusta la ruta si está en otra carpeta
-
+ import GAIA_ABI from "./components/GAIA_ABI.json";
 const GAIA_CONTRACT_ADDRESS = "0x9469260538446129534FC0A0a36251C96Ebd2682"; // Gnosis Mainnet
 
 export function getGAIAContract(provider) {
